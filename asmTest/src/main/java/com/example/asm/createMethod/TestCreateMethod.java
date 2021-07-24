8fff40749f8ae158c957e234b98176a9aae4e3ad
余栋梁 <yudongliang@okay.cn>
2021-07-24 18:07:35 +0800

    remove method

---
+++ asmTest/src/main/java/com/example/asm/createMethod/TestCreateMethod.java
@@ -0,0 +1,40 @@
+package com.example.asm.createMethod;
+
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.MethodNode;
+
+/**
+ * @author yudongliang
+ * create time 2021-07-24
+ * describe :
+ *
+ */
+
+//class MakeMethod {
+//    private var f : Int = 0
+//    fun checkAndSetF(f: Int) {
+//        if (f >= 0) {
+//            this.f = f
+//        } else {
+//            throw IllegalArgumentException()
+//        }
+//    }
+//}
+
+public class TestCreateMethod {
+
+
+
+    public static void main(String[] args) {
+        TestCreateMethod main = new TestCreateMethod();
+        main.exe1();
+    }
+
+
+    private void exe1() {
+        MethodNode methodNode = new MethodNode();
+        InsnList list = methodNode.instructions;
+
+
+    }
+}
