81f449a6102d251ca2c1620e7f172edcd3de36d5
yudongliang <huydl1104@163.com>
2021-08-01 20:39:36 +0800

    plugin 增加threadcanary

---
+++ threadCanary/src/main/java/com/example/threadcanary/ThreadCanaryPlugin.kt
@@ -0,0 +1,10 @@
+package com.example.threadcanary
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ThreadCanaryPlugin :Plugin<Project>{
+    override fun apply(target: Project) {
+
+    }
+}
